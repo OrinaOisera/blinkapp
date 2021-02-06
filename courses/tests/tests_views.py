fd215a39a72ab7b33048e0544785b38dab5675f6
Oisera <orinamark22@gmail.com>
2021-02-07 01:01:07 +0300

    added models unit test

--- courses/tests/tests_views.py
+++ courses/tests/tests_views.py
@@ -8,6 +8,11 @@ from courses.models import  Subject
 class TestViews(TestCase):
   def setup(self):
       self.client = Client()
+      if __name__ == '__main__':
+          self.subject =  Subject.objects.create(title='Programming News')
+          create_sample_user(self.client )
+          looged_in= login_sample_user(user.clinet)
+          self.user = looged_in
 
   def test_home_page(self):
        response = self.client.get('http://127:0.0.1:8000')
