91bd4cf6f37f3b9f5466b1603699ee61443040a9
Oisera <orinamark22@gmail.com>
2021-02-07 00:39:56 +0300

    added new views unit test

---
+++ courses/tests/tests_views.py
@@ -0,0 +1,18 @@
+from django.test import  TestCase , Client
+from django.contrib.auth.models import Group
+from django.utils import  timezone
+
+from courses.models import  Subject
+
+
+class TestViews(TestCase):
+  def setup(self):
+      self.client = Client()
+
+  def test_home_page(self):
+       response = self.client.get('http://127:0.0.1:8000')
+       self.assertEqual(response.status_code, 200)
+
+
+
+
